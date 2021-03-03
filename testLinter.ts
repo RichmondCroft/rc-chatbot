27a7cb70e28dd945eaec3ac252eba411b40e5fc2
unknown <pratikwaghmare000@gmail.com>
2021-03-03 14:15:45 +0530

    Configured Eslint

---
+++ testLinter.ts
@@ -0,0 +1,9 @@
+// console.log(a);
+
+// var a = 2;
+let a = 2;
+
+// let obj2 = { id: 1, age: 102, name: "John", lastName: "Doe", role: "Dev" };
+let obj2 = { id: 1, age: 102, name: 'John', lastName: 'Doe', role: 'Dev' };
+
+let str = 'Winter';
