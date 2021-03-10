b9f515415ca2b9ff3624ae90c4cfcda097eb515f
Digvijay Upadhyay <d.upadhyay@planfwd.de>
2021-03-10 23:46:03 +0100

    feat: configure cypress to test storybook

--- src/Component/Button/Button.tsx
+++ src/Component/Button/Button.tsx
@@ -2,18 +2,25 @@ import React, { ReactNode } from "react";
 import "./button.css";
 
 export interface ButtonProps {
+  id: String;
   type: String;
   children: ReactNode;
   size: String;
   onclick: () => void;
 }
 export const Button: React.FC<ButtonProps> = ({
+  id,
   children,
   type = "primary",
   size = "medium",
   onclick,
 }: ButtonProps) => (
-  <button type="button" className={`button ${type} ${size}`} onClick={onclick}>
+  <button
+    type="button"
+    className={`button ${type} ${size}`}
+    onClick={onclick}
+    id={id}
+  >
     {children}
   </button>
 );
