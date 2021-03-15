211c32b00de45ac9aa87ea3b437b6db037a4e31e
umang <25umangagarwal@gmail.com>
2021-03-15 19:06:46 +0530

    Made styles files for color and size constant

---
+++ src/styles.tsx
@@ -0,0 +1,43 @@
+export const color={
+    primary:"#00b4d8",
+    secondary:"#00b4d8",
+    danger:"#00b4d8",
+    success:"#00b4d8",
+    warning:"#00b4d8",
+}
+export const Box_h_w = {
+      height:{
+        small:"10rem",
+        medium:"30rem",
+        large:"50rem"
+      },
+      width:{
+        small:"10rem",
+        medium:"30rem",
+        large:"50rem"
+      }
+}
+export const Heading_h_w = {
+      height:{
+        small:"3rem",
+        medium:"4rem",
+        large:"6rem"
+      },
+      width:{
+        small:"40rem",
+        medium:"50rem",
+        large:"70rem"
+      }
+}
+export const Button_h_w = {
+      height:{
+        small:"2rem",
+        medium:"3rem",
+        large:"5rem"
+      },
+      width:{
+        small:"10rem",
+        medium:"15rem",
+        large:"20rem"
+      }
+}
