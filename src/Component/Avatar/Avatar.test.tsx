d13412a71a9574b42eb219bce63e3fb0d3cd842b
umang <25umangagarwal@gmail.com>
2021-04-30 16:10:47 +0530

    fix: linting error

--- src/Component/Avatar/Avatar.test.tsx
+++ src/Component/Avatar/Avatar.test.tsx
@@ -1,16 +1,14 @@
-import react from "react";
 import {render,screen} from "@testing-library/react";
 import Avatar from "./Avatar";
 
 describe("Avatar Component",()=>{
     test("Render avatar component to have order of 1",()=>{
-        render(<Avatar order={1}/>)
+        render(<Avatar order={1}/>);
         expect(screen.getByTestId("AvatarComponent")).toHaveAttribute("order","1");
-    })
+    });
     test("Test Avatar component rendering image component",()=>{
-        render(<Avatar order={1}/>)
+        render(<Avatar order={1}/>);
         expect(screen.getByTestId("AvatarImage")).toBeInTheDocument;
-    })
-
+    });
 });
 
