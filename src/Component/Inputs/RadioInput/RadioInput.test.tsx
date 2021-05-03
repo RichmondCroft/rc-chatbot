cca69062bec91cfb8787ebe0aea14eeec8603655
unknown <pratikwaghmare000@gmail.com>
2021-05-03 12:16:57 +0530

    refactor: RadioInput unit tests

--- src/Component/Inputs/RadioInput/RadioInput.test.tsx
+++ src/Component/Inputs/RadioInput/RadioInput.test.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import { render, screen, fireEvent } from "@testing-library/react";
 
 import { RadioInput } from "./RadioInput";
@@ -12,15 +10,17 @@ describe("Checking RadioInput component", () => {
     { id: 3, value: "Option3" },
   ];
 
-  it("should render radio input component", () => {
+  beforeEach(() =>
     render(
       <RadioInput
         name="title"
         options={mockOptions}
         sendMessage={mockSendMessage}
       />
-    );
+    )
+  );
 
+  it("should render radio input component", () => {
     const option1 = screen.getByTestId("RadioInput__Option1");
 
     fireEvent.click(option1);
@@ -30,51 +30,31 @@ describe("Checking RadioInput component", () => {
   });
 
   it("should not send message if no option is selected", () => {
-    render(
-      <RadioInput
-        name="title"
-        options={mockOptions}
-        sendMessage={mockSendMessage}
-      />
-    );
+    const submitButton = screen.getByTestId("RadioInput__submit-btn");
 
-    fireEvent.click(screen.getByTestId("RadioInput__submit-btn"));
+    fireEvent.click(submitButton);
 
     expect(mockSendMessage).not.toHaveBeenCalled();
   });
 
   it("should send message if option is selected", () => {
-    render(
-      <RadioInput
-        name="title"
-        options={mockOptions}
-        sendMessage={mockSendMessage}
-      />
-    );
-
     const option1 = screen.getByTestId("RadioInput__Option1");
+    const submitButton = screen.getByTestId("RadioInput__submit-btn");
 
     fireEvent.click(option1);
     expect(option1).toBeChecked();
 
-    fireEvent.click(screen.getByTestId("RadioInput__submit-btn"));
+    fireEvent.click(submitButton);
 
     expect(mockSendMessage).toHaveBeenCalled();
     expect(mockSendMessage).toHaveBeenCalledWith("Option1");
   });
 
   it("should send message with correct option if options are selected", () => {
-    render(
-      <RadioInput
-        name="title"
-        options={mockOptions}
-        sendMessage={mockSendMessage}
-      />
-    );
-
     const option1 = screen.getByTestId("RadioInput__Option1");
     const option2 = screen.getByTestId("RadioInput__Option2");
     const option3 = screen.getByTestId("RadioInput__Option3");
+    const submitButton = screen.getByTestId("RadioInput__submit-btn");
 
     fireEvent.click(option1);
     expect(option1).toBeChecked();
@@ -87,7 +67,7 @@ describe("Checking RadioInput component", () => {
     expect(option3).not.toBeChecked();
     expect(option1).not.toBeChecked();
 
-    fireEvent.click(screen.getByTestId("RadioInput__submit-btn"));
+    fireEvent.click(submitButton);
 
     expect(mockSendMessage).toHaveBeenCalled();
     expect(mockSendMessage).toHaveBeenCalledWith("Option2");
