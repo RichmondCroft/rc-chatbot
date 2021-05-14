500dba3a52a46e2db7f1396d4056c100d66b1a92
umang <25umangagarwal@gmail.com>
2021-05-14 23:18:42 +0530

    changed the test-id of component

--- src/Component/Heading/Heading.test.tsx
+++ src/Component/Heading/Heading.test.tsx
@@ -5,6 +5,6 @@ import {Heading} from "./Heading";
 describe("Checking Heading component",()=>{
     test("should render heading component with Primary heading",()=>{
        render(<Heading id="primary" size="medium">Primary heading</Heading>);
-       expect(screen.getByTestId("Heading")).toHaveTextContent("Primary heading");
+       expect(screen.getByTestId("Heading_text")).toHaveTextContent("Primary heading");
      });
 });
